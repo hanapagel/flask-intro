daada3693d0b03ab1631eecdf9ed5aae765bea26
Hana Pagel <hana.k.pagel@gmail.com>
2020-02-11 14:20:39 -0800

    Add drop-down menu selection to greet_person().

--- server.py
+++ server.py
@@ -14,11 +14,14 @@ AWESOMENESS = [
     'wonderful', 'smashing', 'lovely']
 
 
+AWFULNESS = [
+    'terrible', 'no-good', 'rotten', 'abominable', 'horrific', 'atrocious']
+
 @app.route("/")
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """<!doctype html><html>This is the Home Page!<a href="localhost:5000/hello">Hello</a></html>"""
 
 
 @app.route("/hello")
@@ -35,7 +38,23 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+
+          How are you feeling?
+
+          <label>Awesome</label>
+          <input type="checkbox" name="compliment" value="awesome"</input>
+
+          <label>Terrific</label>
+          <input type="checkbox" name="compliment" value="terrific"</input>
+
+          <label>Fantastic</label>
+          <input type="checkbox" name="compliment" value="fantastic"</input>
+
+          <label>Neato</label>
+          <input type="checkbox" name="compliment" value="neato"</input>
+
           <input type="submit" value="Submit">
+
         </form>
       </body>
     </html>
@@ -48,9 +67,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
-    y = x
+    compliment = request.args.get("compliment")
 
     return """
     <!doctype html>
@@ -65,7 +82,28 @@ def greet_person():
     """.format(player, compliment)
 
 
+# @app.route("/diss")
+# def insult_person():
+#     """Insult the user by name."""
+
+#     player = request.args.get("person")
+
+#     insult = choice(AWFULNESS)
+
+#     return """
+#     <!doctype html>
+#     <html>
+#         <head>
+#             <title> Hello! </title>
+#         </head>
+#         <body>
+#             Hi, {}! I think you're {}!
+#         </body>
+#     </html>
+#     """.format(player, insult)
+
+
 if __name__ == "__main__":
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
